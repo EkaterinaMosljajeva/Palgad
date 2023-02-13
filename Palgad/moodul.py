eaf58a24cdcf709698fb43908d44851cc2ce3414
EkaterinaMosljajeva <120181296+EkaterinaMosljajeva@users.noreply.github.com>
2023-02-13 14:55:18 +0200

    Update moodul.py

--- Palgad/moodul.py
+++ Palgad/moodul.py
@@ -99,3 +99,137 @@ def vordsed_palgad(i:list,p:list):
             nimi=i[k]
             print(nimi)
 
+def poisk(i:list,p:list):
+    """Tehke palgaotsing isiku nime järgi.
+    :param list i: Inimeste järjend
+    :param list p: Palgade järjend
+    """
+    nimi=input("Kelle palk tahad leia? ")
+    while nimi not in i:
+        nimi=input("Palun kirjuta õige nimi ")
+    n=i.count(nimi)
+    if n!=1:
+        print(f"Siin on mõned inimesed kes nimi on {nimi}") 
+        kopia=i.copy()
+        for j in range(n):
+            ind=kopia.index(nimi)
+            kopia.remove(nimi)
+            kopia.insert(ind,"")
+            print(f"{j+1} {nimi} saab {p[ind]}")
+    else:
+        ind=i.index(nimi)
+        print(f"{nimi} saab {p[ind]}")
+
+def palgadfilter(i:list, p:list):
+    """Leida kõik inimesed, kelle palk on suurem/väiksem kui sisestatud palk.
+    ::param: list i: Inimeste järjend
+    ::param: list p: Palgade järjend
+    """
+    o=int(input("Palk 1-suurem, 2-vähem?"))
+    palk1=int(input("print palk"))
+    list1 = []
+    if o==1:
+        for j in range(len(i)):
+            if palk1<p[j]:
+                list1.append((i[j], p[j]))
+        print(f"kõik inimesed, kes teenivad rohkem kui {palk1} on {list1}")
+    elif o==2:
+        for j in range(len(i)):
+            if palk1>p[j]:
+                list1.append((i[j], p[j]))
+        print(f"kõik inimesed, kes teenivad vähem kui {palk1} on {list1}")
+
+def top3(i:list,p:list): 
+    """3 kõige vaesemat ja rikkamat inimest
+    :param list i: Inimeste järjend
+    :param list p: Palgade järjend
+    """
+    kopia=p.copy()
+    for j in range(3):
+        ind=kopia.index(min(kopia))
+        print(f"{j+1} inimene - {i[ind]} saab väikse palk: {p[ind]}")
+        kopia.pop(ind)
+        kopia.insert(ind,max(p)+1)
+    kopia=p.copy()
+    for j in range(3):
+        ind=kopia.index(max(kopia))
+        print(f"{j+1} inimene - {i[ind]} saab suur palk: {p[ind]}")
+        kopia.pop(ind)
+        kopia.insert(ind,min(p)+1)
+
+def keskmine(i:list,p:list): 
+    """Keskmine palk ja selle saaja nimi
+    :param list i: Inimeste järjend
+    :param list p: Palgade järjend
+    """
+    kesk=sum(p)/len(p)
+    print(f"Keskmine palk on {kesk}")
+    for j in range(len(j)):
+        if p[j]>=kesk:
+            print(f"{i[j]} saab suurem kui keskmine palk, ta saab {p[j]}")
+
+def tulumaks(i:list, p:list):
+    """Arvutage töötasu, mida inimene saab pärast tulumaksu arvutamist.
+    :param: List i: People's list
+    :param: List p: Salaries list
+    """
+    print(i)
+    nimi=int(input("Valige selle isiku ametikoht, kelle palka soovite teada saada: "))
+    nimi2=nimi-1
+    palg=p[nimi2]
+    if palg<=1200:
+        palg-=500
+        if palg < 0:
+            palg = 0
+            palg+=p[nimi2]
+            print(f"{palg}")
+        else:
+            palg*=0.8
+            palg += 500
+            print(f"{palg}")
+    elif palg > 1200 and palg < 2100:
+        tulemaks = 500-0.55556*(palg-1200)
+        palg-=tulemaks
+        palg*=0.8
+        palg+=tulemaks
+        palg= round(palg,1)
+        print(f"{palg}")
+    else:
+        palg*=0.8
+        print(f"{palg}")
+
+def alla_keskmise(i:list,p:list): 
+    """Leidke üles need, kes teenivad alla keskmise palga, ja eemaldage nad nimekirjadest.
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    kesk_palk=sum(p)/len(p)
+    v=int(input("Kellel palk 1-on suurem,2-on väiksem?"))
+    if v==1:
+        for palk in p:
+            if palk>kesk_palk:
+                ind=p.index(palk)
+                p.remove(palk)
+                i.pop(ind)
+    else:
+        for palk in p:
+            if palk<kesk_palk:
+                ind=p.index(palk)
+                p.remove(palk)
+                i.pop(ind)
+    return i,p
+
+def protsent_5(i:list,p:list): 
+    """Igal aastal saavad ettevõtte töötajad 5% palgatõusu, mis on T aasta pärast teatud töötaja palk/palgad.
+    :param list x: Inimeste järjend
+    :param list y: Palgade järjend
+    :rtype:str
+    """
+    aasta=input("Pärast seda, kui palju aastaid palka näidata? ")
+    for j in range(0,len(i)):
+        p1=p[j]
+        for jj in range (j,int(aasta)):
+            p1*=1.05
+        print(f"{i[p]} palk {aasta} aasta pärast on {p1} eurot")
+    return i,p
